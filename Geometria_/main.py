78cae18de3b8e5be5c99ffc2856405b7f05bd712
aac41a <aad41a@gmail.com>
2023-10-31 16:18:07 +0100

    Añadidos comentarios

--- Geometria_/main.py
+++ Geometria_/main.py
@@ -1,6 +1,8 @@
 from Geometria_.model.Geometria import Geometria
 from Geometria_.view.View import View
 
+#comentarios
+
 if __name__ == '__main__':
     g = Geometria(2.00, 3.10, 4.00)
     v = View()
